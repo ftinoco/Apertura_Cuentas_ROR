b7fcee0ffb2206f582912b266c97fcc00411b5c6
Fernando Alberto Tinoco Cerda <ftinoco@XOLOAD2008.COM.NI>
2018-07-10 14:08:58 -0600

    Se removió todo :V

--- app/models/apertura_cuentas_connection.rb
+++
@@ -1,5 +0,0 @@
-class AperturaCuentasConnection < ActiveRecord::Base
-  self.abstract_class = true
-
-  establish_connection :"apc_#{Rails.env}"
-end
--- app/models/datos_cliente.rb
+++
@@ -1,4 +0,0 @@
-class DatosCliente < AperturaCuentaConnection
-	self.table_name = "datos_cliente"
-	self.primary_key = "id_dp"
-end
